428b32e908be1abfcf09fb15814925f35e4da2b7
Yaser Martinez Palenzuela <yaser@imac>
2014-11-25 17:59:20 +0100

    Initial commit

---
+++ conda/conda_install.sh
@@ -0,0 +1,77 @@
+URL=http://repo.continuum.io/miniconda/Miniconda3-3.7.3-Linux-x86_64.sh
+
+# Install dir
+PREFIX=$HOME/miniconda3
+mkdir -p $PREFIX
+
+# Download Miniconda
+wget $URL -P $PREFIX
+
+# Extract packages
+cd $PREFIX
+LINES=$(cat Miniconda* | grep 'MD5=' | cut -d' ' -f3)
+tail -n $LINES Miniconda* | tar -xvf -
+
+extract_dist()
+{
+    echo "installing: $1 ..."
+    DIST=pkgs/$(basename $1 .tar.bz2)
+    mkdir -p $DIST
+    bunzip2 -c ${DIST}.tar.bz2 | tar xvf - -C $DIST
+    rm -f ${DIST}.tar.bz2
+}
+
+for x in pkgs/*.tar.bz2; do extract_dist $x; done
+
+# Install minicoda
+mkdir $PREFIX/envs
+mkdir $HOME/.continuum 2>/dev/null
+
+PYTHON="$(ls $PREFIX/pkgs/python-*/bin/python) -E"
+CONDA_INSTALL=$(ls $PREFIX/pkgs/conda-*/lib/python*/site-packages/conda/install.py)
+$PYTHON $CONDA_INSTALL --prefix=$PREFIX --pkgs-dir=$PREFIX/pkgs --link-all
+
+# config
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PREFIX/bin
+conda config --set always_yes True
+
+# this is needed for libraries like sklearn
+conda install -c https://conda.binstar.org/zhenxieit libstdcplus
+
+## clean to reduce image size
+
+# delete tests
+find . -type d -name tests -exec rm -rf {} \;
+find . -type d -name test -exec rm -rf {} \;
+
+# remove .pyc
+find . -name \__pycache__ -exec rm -rf {} \;
+
+# remove pkgs cache
+rm -r pkgs/*
+
+#remove installer
+rm Miniconda*.sh
+
+
+# Experimental alternative cleaning by choosing .py or .pyc according to size
+# for x in `find . -name *.py`;
+# do
+# dir=$(dirname $x);
+# name=$(basename $x .py);
+# pyc=$dir/__pycache__/$name.cpython-34.pyc;
+# if [ -e $pyc ]; then
+# pycsize=$(wc -c < "$pyc");
+# pysize=$(wc -c < "$x");
+# if (( $pysize > $pycsize )); then
+# if [ -s $x ];then
+# rm $x
+# fi
+# else
+# if [ -s $pyc ];then
+# rm $pyc
+# fi
+# fi
+# fi
+# done
+
---
+++ pyrun/2.7/README.md
@@ -0,0 +1,50 @@
+docker-pyrun
+============
+
+A small(the smallest?) docker container for python.
+
+The total image size is **18.44 MB**. This is achieved by using the great [progrium/busybox](https://github.com/progrium/busybox)
+ as base image and the super small [Egenix PyRun](https://www.egenix.com/products/python/PyRun/).
+
+### Usage:
+
+```bash
+$ docker run -t -i --rm elyase/pyrun python
+eGenix PyRun 2.7.8 (release 2.0.1, default, Aug 26 2014, 11:43:05)
+[GCC 4.6.4]
+Thank you for using eGenix PyRun. Type "help" or "license" for details.
+
+>>>
+```
+
+For a new image create a `Dockerfile` in your Python app project:
+
+```
+FROM elyase/mini-python:2.7
+CMD [ "python", "./your-daemon-or-script.py" ]
+```
+
+### FAQ:
+
+**Why?:**
+
+The official docker python image is too big, the smallest python docker image runs at several hundred MBs. The reason
+for that is that a typical Ubuntu base image is more than 200MB in size plus ~100Mb for a basic python installation.
+
+**When is this useful?:**
+
+You have a small pure python script that you want to test/run in an isolated docker container.
+
+**Which python version is installed in the container?:**
+
+There are two image tags, one for python 2.7 and one for python 3.4.
+
+**What are the drawbacks?:**
+
+Please refer to [PyRun's](https://www.egenix.com/products/python/PyRun/) page. Esentially it is somewhat difficult to install
+dependencies and some less used modules are missing like `readline`, `tkinter`, `multiprocessing`, etc. In
+general everything should run like in a standard python distribution.
+
+**Can I install packages?**
+
+No, but I am planning on also making a pip compatible image.
